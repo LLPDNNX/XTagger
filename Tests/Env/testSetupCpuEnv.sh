0710b2d435dd46c4fae76c83d2925be7c96cf509
matt-komm <Matthias.Komm@cern.ch>
2018-04-27 16:38:49 +0100

    fix path

--- Tests/Env/testSetupCpuEnv.sh
+++ Tests/Env/testSetupCpuEnv.sh
@@ -2,8 +2,8 @@
 
 function execute() 
 {
-    source Training/Env/setupEnvCPU.sh "testenv_cpu"
-    source Training/Env/env_cpu.sh || return 1
+    source Env/setupEnvCPU.sh "testenv_cpu"
+    source Env/env_cpu.sh || return 1
     source activate tf_cpu || return 1
     python Tests/Env/testKeras.py || return 1
     source deactivate tf_cpu || return 1

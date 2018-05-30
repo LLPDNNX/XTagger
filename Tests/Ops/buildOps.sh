6609effdedfc6f895bf82e6bd5fd646433aef796
matt-komm <Matthias.Komm@cern.ch>
2018-05-30 14:46:39 +0100

    add missing install

--- Tests/Ops/buildOps.sh
+++ Tests/Ops/buildOps.sh
@@ -9,6 +9,7 @@ function execute()
     cd Ops/build || return 1
     cmake -DCMAKE_INSTALL_PREFIX=$PROJECTDIR/Ops/release .. || return 1
     make || return 1
+    make install || return 1
     export PYTHONPATH=$PROJECTDIR/Ops/release/lib/python2.7/site-packages
     python -c "import xtagger" || return 1
 }

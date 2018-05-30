0dad023555101adda10f531fa4c87d020b4c86b3
Vilius <vilius.cepaitis@gmail.com>
2018-05-30 15:55:16 +0100

    training tests

---
+++ Tests/Training/Training.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function execute() 
+{
+    PROJECTDIR=$PWD
+    source Env/env_cpu.sh || return 1
+    source activate tf_cpu || return 1
+    git clone https://github.com/LLPDNNX/test-files || return 1
+    python Training/training.py -b 100 --train test-files/nanox_unpacked/train.txt --test test-files/nanox_unpacked/test.txt -e 0 --name test 
+}
+
+execute

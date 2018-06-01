1f7a0c18d5e49d6001d20d3058ea382a22a20abb
Vilius <vilius.cepaitis@gmail.com>
2018-06-01 11:44:17 +0100

    fix 2

--- Tests/Training/Training.sh
+++ Tests/Training/Training.sh
@@ -5,6 +5,7 @@ function execute()
     PROJECTDIR=$PWD
     source Env/env_cpu.sh || return 1
     git clone https://github.com/LLPDNNX/test-files || return 1
+    export PYTHONPATH=$PROJECTDIR/Ops/release/lib/python2.7/site-packages
     python Training/training.py -b 100 --train test-files/nanox_unpacked/train.txt --test test-files/nanox_unpacked/test.txt -e 0 --name test 
 }
 

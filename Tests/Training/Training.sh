51257a4465646b5ffbeee3dacb7380bfadcfe428
Vilius <vilius.cepaitis@gmail.com>
2018-06-13 12:59:46 +0100

    reduce batch size

--- Tests/Training/Training.sh
+++ Tests/Training/Training.sh
@@ -6,7 +6,7 @@ function execute()
     source Env/env_cpu.sh || return 1
     git clone https://github.com/LLPDNNX/test-files || return 1
     export PYTHONPATH=$PYTHONPATH:$PROJECTDIR/Ops/release/lib/python2.7/site-packages
-    python Training/training.py -b 20 --train test-files/nanox_unpacked/train.txt --test test-files/nanox_unpacked/test.txt -e 1 --name test --overwrite -e 10
+    python Training/training.py -b 10 --train test-files/nanox_unpacked/train.txt --test test-files/nanox_unpacked/test.txt -e 1 --name test --overwrite -e 10
 }
 
 execute

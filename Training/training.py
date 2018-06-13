a2cfc00e1eea178fff1e20c9504d7f18fddf47e4
matt-komm <Matthias.Komm@cern.ch>
2018-06-13 19:54:11 +0100

    adaptive reader batch

--- Training/training.py
+++ Training/training.py
@@ -319,7 +319,7 @@ def input_pipeline(files, batchSize):
         resamplers = []
         for _ in range(min(len(fileListTrain)-1, 6)):
             reader = root_reader(fileListQueue, featureDict,
-                                 "jets", batch=10000).batch()
+                                 "jets", batch=min(10,int(batchSize/50.)).batch()
 
             rootreader_op.append(reader)
 

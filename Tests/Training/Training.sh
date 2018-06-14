d801ad5159ede2a708c45f77699a4799374d2eb0
Vilius Cepaitis <vilius.cepaitis@gmail.com>
2018-06-14 11:29:44 +0100

    Train for 1 epoch for the moment

--- Tests/Training/Training.sh
+++ Tests/Training/Training.sh
@@ -7,7 +7,7 @@ function execute()
     git clone https://github.com/LLPDNNX/test-files || return 1
     export PYTHONPATH=$PYTHONPATH:$PROJECTDIR/Ops/release/lib/python2.7/site-packages
     
-    python Training/training.py -b 10 --train test-files/nanox_unpacked/train.txt --test test-files/nanox_unpacked/test.txt -e 2 --name test || return 1 
+    python Training/training.py -b 10 --train test-files/nanox_unpacked/train.txt --test test-files/nanox_unpacked/test.txt -e 1 --name test || return 1 
     python Training/convert_to_const_graph.py output/test/epoch_0/model_epoch0.hdf5 || return 1
 }
 

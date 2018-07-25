0f5e0290328c105e4be2c1793a786b7d9f94f642
Vilius <vilius.cepaitis@gmail.com>
2018-07-25 12:44:58 +0100

    fixes

--- Tests/Training/Training.sh
+++ Tests/Training/Training.sh
@@ -10,7 +10,7 @@ function execute()
     python Training/training.py --name parametric -b 50 -c -p --train test-files/nanox_unpacked/train.txt --test test-files/nanox_unpacked/test.txt -e 2 || return 1 
     python Training/training.py --name ctaux -b 50 -c --train test-files/nanox_unpacked/train.txt --test test-files/nanox_unpacked/test.txt -e 2 || return 1 
     python Training/training.py --name nobalance -b 50 --train test-files/nanox_unpacked/train.txt --test test-files/nanox_unpacked/test.txt -e 2 || return 1 
-    python Training/training.py --name model -m no_conv -b 50 --train test-files/nanox_unpacked/train.txt --test test-files/nanox_unpacked/test.txt -e 2 || return 1 
+    python Training/training.py --name model -m no_lstm -b 50 --train test-files/nanox_unpacked/train.txt --test test-files/nanox_unpacked/test.txt -e 2 || return 1 
     python Training/convert_to_const_graph.py output/ctaux/epoch_1/model_epoch.hdf5 || return 1
     python Training/convert_to_const_graph.py -p output/parametric/epoch_1/model_epoch.hdf5 || return 1
 }
